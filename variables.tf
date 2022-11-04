6498e67a2fdefcc9be717959527b6ebfeda6c458
imgoryachev <bloodon@yandex.ru>
2022-11-04 22:40:51 +0400

    add var

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "instance_name" {
+        description = "Instance name"
+        default = "pc1"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "subnet_id" {
+        description = "VPC subnet"
+        default = "subnet-0c3b88d08d2ebd335"
+}
+
+variable "ami_id" {
+        description = "AMI"
+        default = "ami-08c40ec9ead489470"
+}
+
+variable "number_of_instances" {
+        description = "number of instances"
+        default = 1
+}
+
+
+variable "ami_key_pair_name" {
+        default = "ansinstances"
+}
